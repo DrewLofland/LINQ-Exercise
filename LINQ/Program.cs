7a3e7bbf855c398edca1d0b36bef2ccfe3582427
Drew Lofland <alofland25@gmail.com>
2023-01-18 11:33:10 -0500

    exercise complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LINQ
+﻿using System.Linq;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var games = new List<string>() { "MegaMan X", "Donkey Kong", "Mario Cart", "Goldeneye 007", "Doom" };
+            var orderedGames = games.OrderBy(x => x.Length); 
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine($" {game}");
+            }
         }
     }
 }
